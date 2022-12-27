c8323aac0c99241c0f11faf93917493bf916f76e
Matt Jaeh <matt@jaehdawg.com>
2022-12-27 16:38:17 -0600

    added GA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,9 @@
 import '../styles/global.css';
-
+import { GoogleAnalytics } from "nextjs-google-analytics";
 import type { AppProps } from 'next/app';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
+  <GoogleAnalytics trackPageViews />
   <Component {...pageProps} />
 );
 
