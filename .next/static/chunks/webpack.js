032d1699c94b7aee5a5a91f60b886b9089b7997f
Matt Jaeh <matt@jaehdawg.com>
2022-12-28 13:30:43 -0600

    fix contact form styling

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7f0251a8213d43f3"; }
+/******/ 		__webpack_require__.h = function() { return "d12b85765eedeffa"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -50,22 +50,22 @@ const Contact: NextPage = () => {
                         GET IN CONTACT
                     </h1>
             <div className="max-w-5xl mx-auto py-3">
-                <form className="py-4 space-y-4" onSubmit={handleSubmit}>
+                <form className="py-4 space-y-3" onSubmit={handleSubmit}>
                     <div className="flex items-center justify-center text-black">
                         <label htmlFor="name" className="sr-only">Name</label>
-                        <input value={name} onChange={e => setName(e.target.value)} type="text" name="name" id="name" className="shadow-md focus:ring-indigo-500 focus:border-indigo-500 block w-64 sm:text-md border-gray-300 rounded-md" placeholder="Your Name" />
+                        <input value={name} onChange={e => setName(e.target.value)} type="text" name="name" id="name" className="p-1 shadow-md focus:ring-indigo-500 focus:border-indigo-500 block w-64 sm:text-md border-gray-300 rounded-sm" placeholder="Your Name" />
                     </div>
                     <div className="flex items-center justify-center text-black">
                         <label htmlFor="email" className="sr-only">Email</label>
-                        <input value={email} onChange={e => setEmail(e.target.value)} type="email" name="email" id="email" className="shadow-md focus:ring-indigo-500 focus:border-indigo-500 block w-64 sm:text-md border-gray-300 rounded-md" placeholder="Your Email" />
+                        <input value={email} onChange={e => setEmail(e.target.value)} type="email" name="email" id="email" className="p-1 shadow-md focus:ring-indigo-500 focus:border-indigo-500 block w-64 sm:text-md border-gray-300 rounded-sm" placeholder="Your Email" />
                     </div>
                     <div className="flex items-center justify-center text-black">
                         <label htmlFor="phone" className="sr-only">Phone</label>
-                        <input value={phone} onChange={e => setPhone(e.target.value)} type="tel" name="phone" id="phone" className="shadow-md focus:ring-indigo-500 focus:border-indigo-500 block w-64 sm:text-md border-gray-300 rounded-md" placeholder="Your Phone" />
+                        <input value={phone} onChange={e => setPhone(e.target.value)} type="tel" name="phone" id="phone" className="p-1 shadow-md focus:ring-indigo-500 focus:border-indigo-500 block w-64 sm:text-md border-gray-300 rounded-sm" placeholder="Your Phone" />
                     </div>
                     <div className="flex items-center justify-center text-black">
                         <label htmlFor="message" className="sr-only">Message</label>
-                        <textarea value={message} onChange={e => setMessage(e.target.value)} id="message" className="shadow-md focus:ring-indigo-500 focus:border-indigo-500 block w-64 sm:text-md border-gray-300 rounded-md" placeholder="Your Message" />
+                        <textarea value={message} onChange={e => setMessage(e.target.value)} id="message" className="p-1 shadow-md focus:ring-indigo-500 focus:border-indigo-500 block w-64 h-36 sm:text-md border-gray-300 rounded-sm" placeholder="Your Message" />
                     </div>
                     <div className="flex items-center justify-center text-black">
                         <button type="submit" className="flex items-center justify-center text-sm w-64 rounded-md shadow py-3 px-2 text-white bg-blue-600">SEND</button>
