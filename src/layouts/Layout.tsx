005e52cfb139f4d7b30a4d55fe795e3460ae63dd
Matthew Jaeh <mjaeh@Matthews-MacBook-Pro-2.local>
2025-12-04 14:05:28 -0600

    fixed build errors

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react';
 import { Navbar } from '@/components/Navbar';
 import { Footer } from '@/components/Footer';
-import { Meta } from './Meta';
+
 
 type LayoutProps = {
     meta: ReactNode;
