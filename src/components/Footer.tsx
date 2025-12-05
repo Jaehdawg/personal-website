783e2fd652c2d5af9717575b6778bdfd8e184375
Matthew Jaeh <mjaeh@Matthews-MacBook-Pro-2.local>
2025-12-05 09:53:03 -0600

    icon fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,11 +6,11 @@ const Footer = () => {
             <div className="mx-auto max-w-7xl px-4 text-center sm:px-6 lg:px-8">
                 <h2 className="mb-6 text-2xl font-bold">Connect with me</h2>
                 <div className="mb-8 flex justify-center space-x-6">
-                    <SocialIcon url="https://www.tiktok.com/@realmattjaeh/" bgColor="transparent" />
-                    <SocialIcon url="https://www.instagram.com/jaehdawg/" bgColor="transparent" />
-                    <SocialIcon url="https://www.youtube.com/@mattjaeh/" bgColor="transparent" />
-                    <SocialIcon url="https://www.linkedin.com/in/matthewjaeh/" bgColor="transparent" />
-                    <SocialIcon url="https://www.facebook.com/matthew.jaeh/" bgColor="transparent" />
+                    <SocialIcon url="https://www.tiktok.com/@realmattjaeh/" />
+                    <SocialIcon url="https://www.instagram.com/jaehdawg/" />
+                    <SocialIcon url="https://www.youtube.com/@mattjaeh/" />
+                    <SocialIcon url="https://www.linkedin.com/in/matthewjaeh/" />
+                    <SocialIcon url="https://www.facebook.com/matthew.jaeh/" />
                 </div>
                 <p className="text-sm text-gray-500">
                     © {new Date().getFullYear()} Matt Jaeh. All rights reserved.
